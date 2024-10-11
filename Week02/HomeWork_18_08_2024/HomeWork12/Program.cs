36e4ac7fb5290dae463c6a10158051f5308f90e1
KutsalOzan <kutsalo@hotmail.com>
2024-10-11 22:15:09 +0300

    Week02
    Odev yuklendi

---
+++ Week02/HomeWork_18_08_2024/HomeWork12/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float sayi1 = 1.23f;
+        double sayi2 = (double)sayi1;
+        System.Console.WriteLine(sayi1);
+        System.Console.WriteLine(sayi2);
+    }
+}
